4cb779e9f5966e4e44ed76103cc50805e874f801
George <gosha3484@gmail.com>
2025-11-27 14:08:24 +0200

    added

--- src/main/java/DemoApp.java
+++ src/main/java/DemoApp.java
@@ -3,7 +3,7 @@ public class DemoApp {
     public static void main(String[] args) {
             System.out.println("Demo app is running...");
             try {
-                Thread.sleep(2000); // simulate workload
+                Thread.sleep(2000); // simulate  workload
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
